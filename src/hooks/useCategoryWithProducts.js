85abb245a4551358a0c55d981e7ac70a38524b76
Hari <113083576+Harrijah@users.noreply.github.com>
2026-08-18 15:36:13 +0300

    hooks

---
+++ src/hooks/useCategoryWithProducts.js
@@ -0,0 +1,31 @@
+import {useMemo} from "react";
+import {useSelector} from "react-redux";
+
+export const useCategoryWithProducts = (rayonSelect) => {
+    const allproducts = useSelector((state) => state.productReducer.products);
+    const categoryList = useSelector((state) => state.categorieReducer.categorie);
+
+    const filteredCategory = useMemo(() => {
+        if (!allproducts || !categoryList) return [];
+
+        // lister les IDs de catégories où il y a des produits
+        const categoryIds = new Set(
+            allproducts.map((catid) => catid.categorie)
+        );
+    
+        // filtrer les catégories où il y a des produits
+        const remainingCategory = categoryList.filter((cat) => categoryIds.has(cat.id));
+        
+        return remainingCategory
+            .filter((cat) => cat.rayon == rayonSelect)
+            .map((cat) => (
+                <option key={cat.id} value={cat.id}>
+                    {cat.categorie}
+                </option>
+            ));
+    }, [allproducts, categoryList, rayonSelect]);
+
+    return filteredCategory;
+
+
+}
